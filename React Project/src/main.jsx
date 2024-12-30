be59bb02cf7f17c4e272dc07f2dd56c01c824015
Imran Ahmad <ahmadimrankh99@gmail.com>
2024-12-30 21:05:59 +0500

    committed

--- React Project/src/main.jsx
+++ React Project/src/main.jsx
@@ -1,17 +1,39 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Use 'react-router-dom' for React Router v6
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Use 'react-router-dom' for React Router v6
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import App from './App.jsx';
+import About from './Pages/About.jsx';
+import Home from './Pages/Home.jsx';
 
-const root = document.getElementById('root');
+const router = createBrowserRouter([
+  {
+    path:'/',
+    //element: {user ? <Navigate to="/dashboard" : <Login/>}
+    element: <Home/>
+  },
+  {
+    path:'/about',
+   element: <About/>
+  }
+  //,
+  // {
+  //   path:'/signin',
+  //   element:<Signin/>
+  // },
+  // {
+  //   path:'/dashboard',
+  //   element:<Dashboard/>
+  // }
 
-createRoot(root).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+
+)
